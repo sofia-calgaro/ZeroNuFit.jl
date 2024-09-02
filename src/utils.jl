c4b81aff071b72e4bbb62ebd50ec1de914acb563
Sofia Calgaro <sofiacal@login25.chn.perlmutter.nersc.gov>
2024-09-02 02:33:14 -0700

    introduced a way to retrieve partitions

---
+++ src/utils.jl
@@ -0,0 +1,68 @@
+using LegendDataManagement
+using PropertyFunctions
+using JSON
+using PropDicts
+
+function get_partitions(config::Dict{String, Any}, printflag=nothing)
+    # retrieve metadata path from the config; if nothing, load the online metadata
+    metapath = config["meta_path"]
+    if metapath != nothing
+        meta = LegendDataManagement.AnyProps(metapath)
+    else
+        meta = LegendDataManagement.AnyProps() ### TO DO: test if it works
+    end
+
+    partitions = meta.datasets.ovbb_partitions_pars
+    function printdb(db)
+        if db isa PropDict
+            for (key,value) in db
+                println(key)
+                printdb(value)
+            end
+        else
+            println("\t",db)
+            println()
+        end
+    end
+    
+    # loop over detectors
+    for (detector, detdata) in partitions
+        if detector == :default
+            continue
+        end
+
+        if printflag != nothing
+            println("detector = $detector")
+            println(">>>>>>>>>>> ")
+        end
+        
+        # apply defaults
+        if partitions.default isa PropDicts.MissingProperty 
+            detdata_merge = copy(detdata)
+        else
+            new = partitions.default
+            detdata_merge = merge(new,copy(detdata))
+        end
+
+        # loop over partitions for this detector
+        for (partition, pardata) in detdata_merge
+            if partition == :default
+                continue
+            end
+
+            if detdata_merge.default isa PropDicts.MissingProperty 
+                pardata_merge = copy(pardata)
+            else
+                new = detdata_merge.default
+                pardata_merge = merge(new,copy(pardata))
+            end
+
+            if printflag != nothing
+                println("for partition $partition ")
+                printdb(pardata_merge)
+                println("\n")
+            end
+
+        end
+    end
+end
