534571524d2640756fbb25af49d96708a2161a1a
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-09-02 22:42:25 +0100

    implement reading partitions into a TypedTable

--- src/utils.jl
+++ src/utils.jl
@@ -2,6 +2,44 @@ using LegendDataManagement
 using PropertyFunctions
 using JSON
 using PropDicts
+using FilePathsBase
+using DataStructures
+using PropDicts
+using Tables
+using TypedTables
+using LegendDataManagement
+
+function get_partitions_new(part_path::String)
+    """
+    Get the partition info from a jason file and save  to a Table
+    """
+        part_data_json = JSON.parsefile(part_path,dicttype=DataStructures.OrderedDict)
+        k = keys(part_data_json[1])
+        arrays=Dict()
+        for key in k
+            arrays[key]=[]
+        end
+        for part in part_data_json
+            for key in k
+                append!(arrays[key],[part[key]])
+                end
+    
+        end
+        
+        #TODO: find a way to make this not hardcoded
+        tab = Table(detector=Array(arrays["detector"]),
+                    start_ts=Array(arrays["start_ts"]),
+                    end_ts=Array(arrays["end_ts"]),
+                    eff_tot=Array(arrays["eff_tot"]),
+                    eff_tot_sigma=Array(arrays["eff_tot_sigma"]),
+                    fwhm=Array(arrays["fwhm"]),
+                    fwhm_sigma=Array(arrays["fwhm_sigma"]),
+                    exposure=Array(arrays["exposure"]),
+                    bias =Array(arrays["bias"]),
+                    bias_sigma =Array(arrays["bias_sigma"]))
+    
+        return tab
+end
 
 function get_partitions(config::Dict{String, Any}, printflag=false)
     # retrieve metadata path from the config; if nothing, load the online metadata
