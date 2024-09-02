2b7ac3945cb397cb05040b82c82dd46d1b7fc5d6
Sofia Calgaro <sofiacal@login25.chn.perlmutter.nersc.gov>
2024-09-02 02:33:14 -0700

    introduced a way to retrieve partitions

--- src/ZeroNuFit.jl
+++ src/ZeroNuFit.jl
@@ -3,6 +3,7 @@ module ZeroNuFit
 
 include("fitting.jl")
 include("plotting.jl")
+include("utils.jl")
 using JSON
 
 export run_analysis
@@ -16,8 +17,9 @@ function run_analysis(config::Dict{String, Any})
     
     l200 = [1953.1427, 1955.2213, 1974.731, 1996.4917, 2016.76, 2040.262, 2095.7217]
     
-    samples_l200_uniform = run_fit(l200,fit_function_uniform,distprod(b=0..20.))
-    bat_report(samples_l200_uniform)
+    #samples_l200_uniform = run_fit(l200,fit_function_uniform,distprod(b=0..20.))
+    #bat_report(samples_l200_uniform)
+    get_partitions(config)
     
     println("...done!")
     return # fit_results
