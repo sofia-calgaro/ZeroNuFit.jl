e5cb8c485899bd630fd13bf5141445512c82b773
Sofia Calgaro <sofiacal@login25.chn.perlmutter.nersc.gov>
2024-09-03 03:11:57 -0700

    included plotting of fits + bat settings in config

--- src/ZeroNuFit.jl
+++ src/ZeroNuFit.jl
@@ -12,12 +12,8 @@ export run_analysis
 function run_analysis(config::Dict{String, Any};output_path::String)
 
     @info "You entered into src/ZeroNuFit.jl"
-    @info "Starting the analysis..."
-    @info "- running analysis with the following configuration:"
     
-
-    
-    @info"Let's try retrieving some partitions ..."
+    @info"Let's retrieve some partitions ..."
     partitions = []
     for part_path  in config["partitions"]
         append!(partitions,[get_partitions_new(part_path)])
@@ -27,14 +23,27 @@ function run_analysis(config::Dict{String, Any};output_path::String)
     for (event_path,part) in zip(config["events"],partitions)
         append!(events,[get_events(event_path,part)])
     end
-    @debug "extracted events", events
-    @info "...done!"
+    @debug "... extracted events:", events
     
-    @info "and now we run a fit"
-    samples_uniform = run_fit_over_partitions(partitions[1],events[1],func=fit_function_uniform,config=config,stat_only=config["stat_only"]) 
+    @info "... and now we run a fit"
+    samples_uniform = run_fit_over_partitions(partitions[1],events[1],func=fit_function_linear,config=config,stat_only=config["stat_only"]) 
     println(samples_uniform)
     println(bat_report(samples_uniform))
-
+    
+    @info "and we plot results"
+    energies = []
+    for (idx_k, part_k) in enumerate(partitions[1])
+        if events[1][idx_k] != Any[]
+            for energy in events[1][idx_k]
+                append!(energies, events[1][idx_k])
+            end
+        end
+    end
+    hist_data = append!(Histogram(1930:2:2190), energies)
+    make_plots(samples_uniform, (:B, :S), hist_data, fit_function_linear, config["output_path"])
+    
+    @info "and we save fit results"
+    save_results(samples_uniform,config["output_path"])
     @info "...done!"
     
     return 
