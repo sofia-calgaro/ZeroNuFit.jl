9943ea03b2ae4f1ec0879c98c9896ce5b382403c
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-09-03 01:24:09 +0100

    fix sample report

--- src/ZeroNuFit.jl
+++ src/ZeroNuFit.jl
@@ -32,7 +32,8 @@ function run_analysis(config::Dict{String, Any};output_path::String)
     
     @info "and now we run a fit"
     samples_uniform = run_fit_over_partitions(partitions[1],events[1],func=fit_function_uniform,config=config,stat_only=config["stat_only"]) 
-    bat_report(samples_uniform)
+    println(samples_uniform)
+    println(bat_report(samples_uniform))
 
     @info "...done!"
     
