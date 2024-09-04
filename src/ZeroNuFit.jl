2d541348d554c32f155511357e333a7b877b8907
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-09-04 15:22:37 +0100

    implemented the multi-experiment fit

--- src/ZeroNuFit.jl
+++ src/ZeroNuFit.jl
@@ -21,19 +21,28 @@ Parameters:
     @info "You entered into src/ZeroNuFit.jl"
     
     @info"Let's retrieve some partitions ..."
-    partitions = []
+    partitions = nothing
+    first=true
     for part_path  in config["partitions"]
-        append!(partitions,[get_partitions_new(part_path)])
+
+        part_tmp,fit_groups =get_partitions_new(part_path) 
+        if (first)
+            partitions=part_tmp
+            first=false
+        else
+            partitions=vcat(partitions,part_tmp)
+        end
     end
+    @info display(partitions)
     @info "... load events"
     events = []
-    for (event_path,part) in zip(config["events"],partitions)
-        append!(events,[get_events(event_path,part)])
+    for event_path in config["events"]
+        append!(events,get_events(event_path,partitions))
     end
     @debug "... extracted events:", events
 
     @info "get which partitions have events"
-    part_event_index = get_partition_event_index(events[1],partitions[1])
+    part_event_index = get_partition_event_index(events,partitions)
     
     # check if you want to overwrite the fit; if no results are present, then fit data
     if config["overwrite"] == true || !isfile(joinpath(config["output_path"],"mcmc_files/samples.h5"))
@@ -41,7 +50,7 @@ Parameters:
         if config["overwrite"] == true
             @info "OVERWRITING THE PREVIOUS FIT!"
         end
-        samples = run_fit_over_partitions(partitions[1],events[1],part_event_index,config=config,stat_only=config["stat_only"]) 
+        samples = run_fit_over_partitions(partitions,events,part_event_index,config=config,stat_only=config["stat_only"]) 
         @info "fit ran succesfully"
     else
         @info "... we load already existing fit results"
