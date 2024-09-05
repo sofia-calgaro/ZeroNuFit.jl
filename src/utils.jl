cca938eb080c7607ce9afc7317effc26793486dd
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-09-05 17:02:50 +0100

    small fix

--- src/utils.jl
+++ src/utils.jl
@@ -181,7 +181,7 @@ Function to plot and save results, as well as inputs
     @info "...done!"
     
     @info "... now we save samples (untouched if we do not want to overwrite)"
-    if !isfile(joinpath(config["output_path"],"mcmc_files/samples.h5"))
+    if config["overwrite"]==true || !isfile(joinpath(config["output_path"],"mcmc_files/samples.h5"))
         save_generated_samples(samples, output_path)
     @info "...done!"
     end
