45509446462a7647de0a73646a7e42140a3a2bc6
Sofia Calgaro <sofiacal@login15.chn.perlmutter.nersc.gov>
2024-09-22 10:16:23 -0700

    fixed likelihood sign

--- src/likelihood.jl
+++ src/likelihood.jl
@@ -132,7 +132,7 @@ free parameters: signal (S), background (B), energy bias (biask) and resolution 
 
             # get the correct reso and bias (
             reso,bias = get_energy_scale_pars(part_k,p,settings,idx_part_with_events)
-            term2 = model_s_k * pdf(Normal(Qbb + bias, reso), evt_energy) 
+            term2 = model_s_k * pdf(Normal(Qbb - bias, reso), evt_energy) 
         else
             term2 =0
         end
@@ -254,7 +254,7 @@ Returns
 
                 reso,bias = get_energy_scale_pars(part_k,p,settings,idx_part_with_events)
 
-                append!(events,rand(Normal(Qbb + bias, reso)))
+                append!(events,rand(Normal(Qbb - bias, reso)))
                 
         
             end
