6ec6e1b9853911f766364fe09e83a6649268d8d7
Sofia Calgaro <sofiacal@login15.chn.perlmutter.nersc.gov>
2024-09-26 03:24:32 -0700

    fixed environment

--- docs/make.jl
+++ docs/make.jl
@@ -1,62 +1,15 @@
-# Use
-#
-#     DOCUMENTER_DEBUG=true julia --color=yes make.jl local [nonstrict] [fixdoctests]
-#
-# for local builds.
+using Pkg
+Pkg.activate("docs")
+Pkg.instantiate()
 
 using Documenter
-using Literate
-using BAT
-
-using DensityInterface
-using Optim
-
-# Import HDF5 only on 64-bit, automated installation of HDF5 doesn't seem to work
-# properly on 32-bit on CI system:
-if Int == Int64
-    import HDF5
-end
-
-
-function fix_literate_output(content)
-    content = replace(content, "EditURL = \"@__REPO_ROOT_URL__/\"" => "")
-    return content
-end
-
-gen_content_dir = joinpath(@__DIR__, "src")
-tutorial_src = joinpath(@__DIR__, "src", "tutorial_lit.jl")
-Literate.markdown(tutorial_src, gen_content_dir, name = "tutorial", documenter = true, credit = true, postprocess = fix_literate_output)
-#Literate.markdown(tutorial_src, gen_content_dir, name = "tutorial", codefence = "```@repl tutorial" => "```", documenter = true, credit = true)
-Literate.notebook(tutorial_src, gen_content_dir, execute = false, name = "bat_tutorial", documenter = true, credit = true)
-Literate.script(tutorial_src, gen_content_dir, keep_comments = false, name = "bat_tutorial", documenter = true, credit = false)
+using ZeroNuFit  # Replace with your package's name
 
 makedocs(
-    sitename = "BAT",
-    modules = [BAT],
-    format = Documenter.HTML(
-        prettyurls = !("local" in ARGS),
-        canonical = "https://bat.github.io/BAT.jl/stable/"
-    ),
-    authors = "The BAT development team",
+    sitename = "ZeroNuFit.jl",
+    authors = "S. Calgaro, T. Dixon",
+    format = :html,
     pages = [
         "Home" => "index.md",
-        "Installation" => "installation.md",
-        "List of algorithms" => "list_of_algorithms.md",
-        "Tutorial" => "tutorial.md",
-        "API Documentation" => "stable_api.md",
-        "Plotting" => "plotting.md",
-        "Experimental Features" => "experimental_api.md",
-        "Internal API" => "internal_api.md",
-        "Developer instructions" => "developing.md",
-        "License" => "license.md",
-    ],
-    doctest = ("fixdoctests" in ARGS) ? :fix : true,
-    linkcheck = "linkcheck" in ARGS,
-    warnonly = ("nonstrict" in ARGS),
-)
-
-deploydocs(
-    repo = "github.com/bat/BAT.jl.git",
-    forcepush = true,
-    push_preview = true,
+    ]
 )
